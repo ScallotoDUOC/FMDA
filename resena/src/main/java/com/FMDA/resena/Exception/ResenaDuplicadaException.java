bf78e99014f44ec7c8d301a4b4fdc7ca667590a3
ScallotoDUOC <ang.sepulvedah@duocuc.cl>
2026-06-01 04:37:11 -0400

    Delete resena directory

--- resena/src/main/java/com/FMDA/resena/Exception/ResenaDuplicadaException.java
+++
@@ -1,7 +0,0 @@
-package com.FMDA.resena.Exception;
-
-public class ResenaDuplicadaException extends RuntimeException {
-    public ResenaDuplicadaException(String usuario) {
-        super("Resena de el usuario " + usuario + " ya está registrada");
-    }
-}
--- resena/src/main/java/com/FMDA/resena/Exception/ResenaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.FMDA.resena.Exception;
-
-public class ResenaNotFoundException extends RuntimeException {
-
-    public ResenaNotFoundException(Long id) {
-        super("Resena con ID " + id + " no encontrada");
-    }
-}
