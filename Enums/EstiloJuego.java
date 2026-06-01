361788ecb74bd81b0cb3b9b9bc52e6bbd1c955a4
ScallotoDUOC <ang.sepulvedah@duocuc.cl>
2026-06-01 04:35:07 -0400

    Delete Enums directory

--- Enums/EstiloJuego.java
+++
@@ -1,8 +0,0 @@
-package AngelYMilton.FMDataAnalisis.Enums;
-
-public enum EstiloJuego {
-
-    GEGENPRESSING,
-    CONTROL_POSESION,
-    BLOQUE_BAJO_DEFENSIVO
-}
--- Enums/PiernaHabil.java
+++
@@ -1,7 +0,0 @@
-package AngelYMilton.FMDataAnalisis.Enums;
-
-public enum PiernaHabil {
-    ZURDO,
-    DIESTRO,
-    AMBIDIESTRO
-}
