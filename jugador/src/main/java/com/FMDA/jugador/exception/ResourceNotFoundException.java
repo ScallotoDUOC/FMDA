18cc7cc68802f4315ce4651274edadaeb4f8b18d
ScallotoDUOC <ang.sepulvedah@duocuc.cl>
2026-06-01 04:36:18 -0400

    Delete jugador directory

--- jugador/src/main/java/com/FMDA/jugador/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.FMDA.jugador.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
